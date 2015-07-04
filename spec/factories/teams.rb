8d204f66e125bd5cf3ade0ac088602529b80ff28
Jake Mays <quaunaut@gmail.com>
2015-07-04 13:30:19 -0600

    Updated factory to support player counts, added serializer.

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -2,5 +2,15 @@ FactoryGirl.define do
   factory :team do
     name { Faker::Company.name }
     location { Faker::Address.country }
+
+    factory :team_with_players do
+      transient do
+        player_count 5
+      end
+
+      after(:create) do |team, evaluator|
+        create_list(:player, evaluator.player_count, team: team)
+      end
+    end
   end
 end
---
+++ spec/serializers/team_serializer_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+# The serializer test framework used here came from Attila Gyorffy
+# here: http://eclips3.net/2015/01/24/testing-active-model-serializer-with-rspec/
+RSpec.describe TeamSerializer, type: :serializer do
+  let(:player_count) { 6 }
+  let(:resource) { FactoryGirl.create(:team_with_players, player_count: player_count) }
+  let(:serializer) { TeamSerializer.new(resource) }
+  let(:serialization) { ActiveModel::Serializer::Adapter.create(serializer) }
+
+  subject do
+    JSON.parse(serialization.to_json)['data']
+  end
+
+  it 'has an id' do
+    expect(subject['id'].to_i).to eq resource.id
+  end
+
+  it 'has the handle' do
+    expect(subject['attributes']['name']).to eq resource.name
+  end
+
+  it 'has relationships including players' do
+    expect(subject['relationships']['players']['data'].length).to eq player_count
+  end
+end
