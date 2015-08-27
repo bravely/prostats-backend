d06bdd9ac465add9e51a46e2d8977ee510547da3
Jake Mays <quaunaut@gmail.com>
2015-08-27 10:24:31 -0600

    Added included models to tournaments controller.

--- app/serializers/game_small_serializer.rb
+++ app/serializers/game_small_serializer.rb
@@ -1,3 +1,7 @@
 class GameSmallSerializer < ActiveModel::Serializer
   attributes :id, :played_at, :notes, :game_length, :game_number, :youtube_url
+
+  belongs_to :blue_team, serializer: TeamSmallSerializer
+  belongs_to :red_team, serializer: TeamSmallSerializer
+  belongs_to :winner, serializer: TeamSmallSerializer
 end
--- app/serializers/match_small_serializer.rb
+++ app/serializers/match_small_serializer.rb
@@ -1,8 +1,3 @@
 class MatchSmallSerializer < ActiveModel::Serializer
   attributes :id, :live, :finished, :name, :played_at, :max_games
-
-  belongs_to :tournament, serializer: TournamentSmallSerializer
-  belongs_to :blue_team, serializer: TeamSmallSerializer
-  belongs_to :red_team, serializer: TeamSmallSerializer
-  belongs_to :winner, serializer: TeamSmallSerializer
 end
