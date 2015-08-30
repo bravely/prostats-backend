a4fa09d780036474936b57cb386aa7bc9c124f03
Jake Mays <quaunaut@gmail.com>
2015-08-30 10:26:40 -0600

    Added starter and contract expiration to Player.

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,7 +1,7 @@
 class PlayerSerializer < ActiveModel::Serializer
   cache key: 'players', expires_in: 1.hour
   attributes :id, :handle, :position, :first_name, :last_name, :season_wins,
-             :season_losses
+             :season_losses, :starter, :contract_expires_at
 
   belongs_to :team, serializer: TeamSmallSerializer
   # has_many :games
--- app/serializers/player_small_serializer.rb
+++ app/serializers/player_small_serializer.rb
@@ -1,5 +1,5 @@
 class PlayerSmallSerializer < ActiveModel::Serializer
   cache key: 'players_small', expires_in: 1.hour
   attributes :id, :handle, :position, :first_name, :last_name, :season_wins,
-             :season_losses
+             :season_losses, :starter, :contract_expires_at
 end
