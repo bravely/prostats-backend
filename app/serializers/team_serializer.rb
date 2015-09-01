7334ca1ec70625b479abb1192d9707a8873b4836
Jake Mays <quaunaut@gmail.com>
2015-09-01 14:48:36 -0600

    Added acronym to serialized payload.

--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,6 +1,6 @@
 class TeamSerializer < ActiveModel::Serializer
   cache key: 'teams', expires_in: 1.hour
-  attributes :id, :name, :location
+  attributes :id, :name, :location, :acronym
 
   belongs_to :league, serializer: LeagueSmallSerializer
   has_many :players, serializer: PlayerSmallSerializer
--- app/serializers/team_small_serializer.rb
+++ app/serializers/team_small_serializer.rb
@@ -1,4 +1,4 @@
 class TeamSmallSerializer < ActiveModel::Serializer
   cache key: 'teams_small', expires_in: 1.hour
-  attributes :id, :name, :location
+  attributes :id, :name, :location, :acronym
 end
