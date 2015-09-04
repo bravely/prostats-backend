5ef9a119c11035f7954a42ca5ce6599edd9b1406
Jake Mays <quaunaut@gmail.com>
2015-09-04 10:17:36 -0600

    Added games index route, including support for pagination.

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,6 +1,20 @@
 class Api::V1::GamesController < ApplicationController
   before_action :find_game, only: [:show]
 
+  def index
+    @games = Game.where(game_params)
+             .order(played_at: :desc)
+             .includes(:plays, :players, :blue_team, :red_team)
+             .page(params[:page])
+             .per(params[:limit] || 10)
+
+    @games = @games.with_player(player_param) if player_param
+
+    render json: @games,
+           include: %w(plays players blue_team red_team winner),
+           meta: { total_pages: @games.total_pages }
+  end
+
   def show
     render json: @game, include: %w(plays players blue_team red_team)
   end
@@ -9,5 +23,13 @@ class Api::V1::GamesController < ApplicationController
 
   def find_game
     @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.permit(:match_id, :finished)
+  end
+
+  def player_param
+    params[:player_id]
   end
 end
--- spec/requests/api/v1/games_spec.rb
+++ spec/requests/api/v1/games_spec.rb
@@ -1,6 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe 'Games API', type: :request do
+  describe 'GET /api/games' do
+    context 'with no query params' do
+      let!(:game_one) { FactoryGirl.create(:game) }
+      let!(:game_two) { FactoryGirl.create(:game) }
+      before do
+        get api_games_path
+      end
+      it { expect(response).to be_success }
+      it { expect(response.content_type).to eq 'application/json' }
+      it { expect(json['data'].map { |e| e['id'].to_i }).to include(game_one.id, game_two.id) }
+    end
+    context 'with query params' do
+      let(:match) { FactoryGirl.create(:match) }
+      let!(:game_with_match) { FactoryGirl.create(:game, match: match) }
+      let!(:game_without) { FactoryGirl.create(:game) }
+      before do
+        get api_games_path match_id: match.id
+      end
+      it { expect(response).to be_success }
+      it { expect(response.content_type).to eq 'application/json' }
+      it { expect(json['data'].map { |e| e['id'].to_i }).to include(game_with_match.id) }
+      it { expect(json['data'].map { |e| e['id'].to_i }).to_not include(game_without.id) }
+    end
+    context 'with player_id query param' do
+      let(:player) { FactoryGirl.create(:player) }
+      let!(:game_without_player) { FactoryGirl.create(:game) }
+      let!(:game_with_player) do
+        FactoryGirl.create(:game).tap do |game|
+          game.players << player
+        end
+      end
+      before do
+        get api_games_path player_id: player.id
+      end
+      it { expect(response).to be_success }
+      it { expect(response.content_type).to eq 'application/json' }
+      it { expect(json['data'].map { |e| e['id'].to_i }).to include(game_with_player.id) }
+      it { expect(json['data'].map { |e| e['id'].to_i }).to_not include(game_without_player.id) }
+    end
+  end
+
   describe 'GET /api/games/:id' do
     let(:game) { FactoryGirl.create(:game_with_full_teams) }
     before do
